a4695ff63103ceb827829abe389ccdd50431df8f
Shivakant <kants6397@gmail.com>
2024-03-15 22:00:41 +0530

    completed

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -4,6 +4,11 @@ import { useState } from "react"
 import Card from "../Card/Card"
 import './Grid.css'
 
+import toast,{Toaster} from "react-hot-toast";
+ 
+//  import 'react-toastify/dist/ReactToastify.css';
+
+
 function isWinner(board, symbol) {
   if (board[0] == board[1]&& board[1] == board[2]&&board[2] == symbol) return symbol;
   if (board[3] == board[4]&& board[4] == board[5]&&board[5] == symbol) return symbol;
@@ -36,22 +41,61 @@ function Grid({ numOfcards }) {
     const win = isWinner(board, turn ? "O" : "X")
     if (win) {
       setWinner(win);
+      toast.success(`Congrotulations ${win} wins the game...`);
     }
     setBoard([...board])
     setTurn(!turn);
+      
+  }
 
+  function reset() {
+    setBoard(Array(numOfcards).fill(""));
+    setWinner(null);
+    setTurn(true)
+    // toast.success(`game reset successfully ....`);
   }
   return (
 
-    <>
-      {winner && <h1>Winner is :{winner }</h1>}
+    <div className="grid-wrapper">
+      
+      {winner && (
+        <>
+          <h1>Winner is :{winner}</h1>
+          <button className="reset" onClick={reset}>Reset game </button>
+         <Toaster
+  position="top-center"
+  reverseOrder={false}
+  gutter={8}
+  containerClassName=""
+  containerStyle={{}}
+  toastOptions={{
+    // Define default options
+    className: '',
+    duration: 5000,
+    style: {
+      background: '#363636',
+      color: '#fff',
+    },
+
+    // Default options for specific types
+    success: {
+      duration: 5000,
+      theme: {
+        primary: 'green',
+        secondary: 'black',
+      },
+    },
+  }}
+/>
+        </>
+      )}
       <h1 className="turn-highlight">Current turn: {(turn)?'O':'X'}   </h1>
     <div className="grid">
       {board.map((value, idx) => {
         return <Card onPlay={play} player={value} key={idx} index={idx} />
     })}
       </div>
-      </>
+      </div>
   )
 }
 
